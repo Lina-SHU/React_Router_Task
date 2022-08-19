9930eb7dd27dd6b027623b322b48775750e98759
LinaChen <linachen@uitc.com.tw>
2022-08-19 10:09:09 +0800

    add post router

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import {
   NavLink,
   Routes,
   Route,
-  useNavigate
+  useNavigate,
+  useParams,
+  Outlet
 } from 'react-router-dom';
 
 const Logout = () => {
@@ -27,6 +29,22 @@ const Register = () => {
   return <p>這是註冊頁面</p>;
 };
 
+const Post = () => {
+  return (
+    <>
+      <p>Post 頁面</p>
+      <Outlet />
+    </>
+  )
+  
+  
+}
+
+const PostId = () => {
+  let param = useParams();
+  return <p>PostID： {param.postId}</p>
+}
+
 function App() {
   return (
     <div className="container">
@@ -44,12 +62,21 @@ function App() {
           <NavLink to="/todo">
             <p>Todo 頁面</p>
           </NavLink>
+          <NavLink to="/post">
+            <p>Post</p>
+          </NavLink>
+          <NavLink to="/post/post123">
+            <p>Post 詳細頁面</p>
+          </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
         <Routes>
           <Route path="/register" element={<Register />}>註冊頁面</Route>
           <Route path="/login" element={<Login />}>登入頁面</Route>
           <Route path="/todo" element={<Todo />}>Todo 頁面</Route>
+          <Route path="/post" element={<Post />}>
+            <Route path=":postId" element={<PostId />}></Route>
+          </Route>
         </Routes>
         {/* 練習區 */}
       </HashRouter>
